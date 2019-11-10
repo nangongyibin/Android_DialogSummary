3482f614a74f7b6adf9b0f2298e681bce82aaf5e
nangongyibin7219 <nangongyibin@gmail.com>
2019-11-10 20:26:02 +0800

    显示默认值及上一次选择的样式

--- app/src/main/java/com/example/dialogsummary/MainActivity.java
+++ app/src/main/java/com/example/dialogsummary/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.dialogsummary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ngyb.dialogsummary.CustomerDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void on(View view) {
+        CustomerDialog customerDialog = new CustomerDialog(this);
+        customerDialog.show();
     }
 }
